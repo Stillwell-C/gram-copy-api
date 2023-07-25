5386d7b8bc800b10bc16b5271e0f84c12c929cd7
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-25 17:16:56 +0900

    Add auth router

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const createServer = () => {
   app.use("/", express.static(path.join(__dirname, "public")));
   app.use("/", require("./routes/root"));
   app.use("/users", require("./routes/userRoutes"));
+  app.use("/auth", require("./routes/authRoutes"));
 
   app.all("*", (req, res) => {
     res.status(404);
