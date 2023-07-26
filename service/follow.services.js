e13718ae6e4009a3b6576aed7dbca78de45ac758
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-26 16:39:18 +0900

    Initial commit

---
+++ service/follow.services.js
@@ -0,0 +1,45 @@
+const Follow = require("../models/Follow");
+
+const findFollow = async (followedID, followerID) => {
+  return Follow.findOne({ followed: followedID, follower: followerID }).exec();
+};
+
+const findAllFollowers = async (userID, populate = false) => {
+  if (populate) {
+    return Follow.find({ followed: userID })
+      .sort("createdAt")
+      .populate("follower", "_id username")
+      .select("-followed")
+      .lean();
+  } else {
+    return Follow.find({ followed: userID }).sort("createdAt").lean();
+  }
+};
+
+const findAllFollowing = async (userID, populate = false) => {
+  if (populate) {
+    return Follow.find({ follower: userID })
+      .sort("createdAt")
+      .populate("followed", "_id username")
+      .select("-follower")
+      .lean();
+  } else {
+    return Follow.find({ follower: userID }).sort("createdAt").lean();
+  }
+};
+
+const createNewFollow = async (followedID, followerID) => {
+  return Follow.create({ followed: followedID, follower: followerID }).exec();
+};
+
+const findAndDeleteFollow = async (followID) => {
+  return Follow.findByIdAndDelete(followID);
+};
+
+module.exports = {
+  findFollow,
+  findAllFollowers,
+  findAllFollowing,
+  createNewFollow,
+  findAndDeleteFollow,
+};
