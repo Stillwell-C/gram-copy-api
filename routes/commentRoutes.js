e6ac59c34728cfcfbd7fbbef5d2cb6f7e147b5bf
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-26 17:17:55 +0900

    Initial commit

---
+++ routes/commentRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const commentsController = require("../controllers/commentsController");
+
+router
+  .route("/")
+  .post(commentsController.createComment)
+  .delete(commentsController.deleteComment);
+
+router.route("/:id").get(commentsController.getComment);
