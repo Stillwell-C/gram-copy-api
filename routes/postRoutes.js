d18c3987421e5ae2250f8c577099cf88f65ec768
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-26 15:36:11 +0900

    Initial commit

---
+++ routes/postRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const postController = require("../controllers/postsController");
+
+router
+  .route("/")
+  .get(postController.getMultiplePosts)
+  .post(postController.createNewPost)
+  .patch(postController.updatePost)
+  .delete(postController.deletePost);
+
+router.route("/:id").get(postController.getPost);
