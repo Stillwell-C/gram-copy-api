2bf15a5078bbf415306421aeb83deff989780fa6
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-27 10:58:56 +0900

    Add postLike & postSave routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/usersController");
+const postsSaveController = require("../controllers/postSaveController");
+const postsLikeController = require("../controllers/postLikeController");
 
 router
   .route("/")
@@ -10,5 +12,9 @@ router
   .delete(usersController.deleteUser);
 
 router.route("/:id").get(usersController.getUser);
+
+router.route("/:id/save").get(postsSaveController.getUsersSavedPosts);
+
+router.route("/:id/like").get(postsLikeController.getUsersLikedPosts);
 
 module.exports = router;
