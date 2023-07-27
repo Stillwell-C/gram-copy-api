4908b57f74b6c5685175968ddbf41070d8db5560
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-27 11:10:32 +0900

    Remove routes not from postController

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postsController");
-const commentsController = require("../controllers/commentsController");
-const postSaveController = require("../controllers/postSaveController");
-const postLikeController = require("../controllers/postLikeController");
 
 router
   .route("/")
@@ -14,16 +11,4 @@ router
 
 router.route("/:id").get(postController.getPost);
 
-router.route("/:id/comments").get(commentsController.getPostComments);
-
-router
-  .route("/:id/save")
-  .get(postSaveController.getAllSavedUsers)
-  .post(postSaveController.createPostSave)
-  .delete(postSaveController.deletePostSave);
-
-router
-  .route("/:id/like")
-  .get(postLikeController.getAllLikedUsers)
-  .post(postLikeController.createPostLike)
-  .delete(postLikeController.deletePostLike);
+module.exports = router;
