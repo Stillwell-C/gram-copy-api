b22aaa55cb058bde1efa8e3eb0e4cb93345d2ea8
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-08-04 14:42:16 +0900

    Add comments property

--- models/Post.js
+++ models/Post.js
@@ -23,6 +23,10 @@ const postSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    comments: {
+      type: Number,
+      default: 0,
+    },
     location: {
       type: String,
       default: "",
