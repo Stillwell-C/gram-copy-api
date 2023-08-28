11320008f6fb03e6069c83fe39b91632f824858e
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-08-28 18:00:41 +0900

    Add usernameAvailability & emailAvailability routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,4 +15,11 @@ router.route("/search/:searchQuery").get(usersController.searchUsers);
 
 router.route("/userArr/:userArr").get(usersController.getUsersFromArr);
 
+router
+  .route("/availability/username/:username")
+  .get(usersController.usernameAvailability);
+router
+  .route("/availability/email/:email")
+  .get(usersController.emailAvailability);
+
 module.exports = router;
