997c7405a78a7eae74942aad098878f575351597
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-09-07 14:47:54 +0900

    Add verify JWT middleware

--- routes/postSaveRoutes.js
+++ routes/postSaveRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const postSaveController = require("../controllers/postSaveController");
+const verifyJWT = require("../middleware/verifyJWT");
 
 router
   .route("/:id/")
   .get(postSaveController.getAllSavedUsers)
-  .post(postSaveController.createPostSave)
-  .delete(postSaveController.deletePostSave);
+  .post(verifyJWT, postSaveController.createPostSave)
+  .delete(verifyJWT, postSaveController.deletePostSave);
 
-router.route("/user/").get(postSaveController.getUsersSavedPosts);
+router.route("/user/").get(verifyJWT, postSaveController.getUsersSavedPosts);
 
 module.exports = router;
