26dded06a4e3aa04e708511229d181ffdd59623a
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-09-07 15:20:02 +0900

    Add verify JWT middleware

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,17 +2,18 @@ const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postsController");
 const selectiveJWTVerification = require("../middleware/selectiveJWTVerification");
+const verifyJWT = require("../middleware/verifyJWT");
 
 router
   .route("/")
   .get(selectiveJWTVerification, postController.getMultiplePosts)
   .post(postController.createNewPost)
-  .patch(postController.updatePost)
-  .delete(postController.deletePost);
+  .patch(verifyJWT, postController.updatePost)
+  .delete(verifyJWT, postController.deletePost);
 
 router.route("/:id").get(postController.getPost);
 
-router.route("/tagged").patch(postController.updateTaggedUsers);
+router.route("/tagged").patch(verifyJWT, postController.updateTaggedUsers);
 
 router.route("/tagged/:userID").get(postController.getTaggedPosts);
 
