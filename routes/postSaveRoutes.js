1a35f7617755e765d8bc26f8f8820256bbddca26
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-09-07 16:38:24 +0900

    Change order of routes

--- routes/postSaveRoutes.js
+++ routes/postSaveRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const postSaveController = require("../controllers/postSaveController");
 const verifyJWT = require("../middleware/verifyJWT");
 
+router.route("/user/").get(verifyJWT, postSaveController.getUsersSavedPosts);
+
 router
   .route("/:id/")
   .get(postSaveController.getAllSavedUsers)
   .post(verifyJWT, postSaveController.createPostSave)
   .delete(verifyJWT, postSaveController.deletePostSave);
-
-router.route("/user/").get(verifyJWT, postSaveController.getUsersSavedPosts);
 
 module.exports = router;
