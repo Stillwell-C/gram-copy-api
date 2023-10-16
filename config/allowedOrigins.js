5bac5979d86817f4d1de9e38d96d8463872783fa
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-16 16:51:06 +0900

    Revert allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,13 @@
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = [
+  "http://gram-copy.vercel.app",
+  "https://gram-copy.vercel.app",
+  "gram-copy.vercel.app",
+  "http://gram-copy-stillwell-c.vercel.app",
+  "https://gram-copy-stillwell-c.vercel.app",
+  "gram-copy-stillwell-c.vercel.app",
+  "http://gram-copy-git-main-stillwell-c.vercel.app",
+  "https://gram-copy-git-main-stillwell-c.vercel.app",
+  "gram-copy-git-main-stillwell-c.vercel.app/",
+];
 
 module.exports = allowedOrigins;
